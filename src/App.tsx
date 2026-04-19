0565865bca1048871c85d737ff1e6be9beb13244
popovaav <popova.av@ukr.net>
2026-04-19 14:44:19 -0300

    implement drag functionality for notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
 import type { Note } from './types/note'
 import { useState } from 'react'
+import type { MouseEvent } from 'react'
 
 function App() {
   const [notes, setNotes] = useState<Note[]>([])
+  const [draggingId, setDraggingId] = useState<string | null>(null)
+  const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 })
 
   const createNote = () => {
     const offset = notes.length * 20
@@ -19,16 +22,50 @@ function App() {
     setNotes(prev => [...prev, newNote])
   }
 
+  const handleMouseDown = (
+    e: MouseEvent<HTMLDivElement>,
+    note: Note
+  ) => {
+    setDraggingId(note.id)
+
+    setDragOffset({
+      x: e.clientX - note.x,
+      y: e.clientY - note.y,
+    })
+  }
+
+  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    if (!draggingId) return
+
+    setNotes(prev =>
+      prev.map(note => {
+        if (note.id !== draggingId) return note
+
+        return {
+          ...note,
+          x: e.clientX - dragOffset.x,
+          y: e.clientY - dragOffset.y,
+        }
+      })
+    )
+  }
+
+  const handleMouseUp = () => {
+    setDraggingId(null)
+    setDragOffset({ x: 0, y: 0 })
+  }
+
   return (
     <main>
       <h1>Sticky Notes</h1>
       <button onClick={createNote}>Add note</button>
 
-      <div className="board">
+      <div className="board" onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}>
         {notes.map(note => (
           <div
             key={note.id}
             className="note"
+            onMouseDown={(e) => handleMouseDown(e, note)}
             style={{
               position: 'absolute',
               left: note.x,
